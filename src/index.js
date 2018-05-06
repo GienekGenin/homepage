6da05343354e61cb04af724373b19863f6ad126d
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-05-06 11:34:14 +0200

    Fixed body background problem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import $ from 'jquery';
 import './scss/style.scss';
 
-$(document).ready(function(){
-
+$(document).ready(function () {
+    $("body").css({
+        "padding-top": "200px",
+        "background": "url(../src/img/mountain.jpg) no-repeat center center",
+        "-webkit-background-size": "cover",
+        "-moz-background-size": "cover",
+        "-o-background-size": "cover",
+        "background-size": "cover",
+        "min-height": "100vh",
+        "width": "100%"
+    });
 });
