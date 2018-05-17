069f00f6c0d36ed3e0d07bcd4682fa612322c374
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-05-17 22:46:19 +0200

    Working on a new design

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import $ from 'jquery';
 import './scss/style.scss';
 
 $(document).ready(function () {
-    $('.container').hide();
-    $("body").css({
-        "padding-top": "200px",
-        "background": "url(../src/img/mountain.jpg) no-repeat center center",
-        "-webkit-background-size": "cover",
-        "-moz-background-size": "cover",
-        "-o-background-size": "cover",
-        "background-size": "cover",
-        "min-height": "100vh",
-        "width": "100%"
+    $("#edu").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".timeline").offset().top
+        }, 2000);
     });
-    $('.container').fadeIn(5000);
 });
